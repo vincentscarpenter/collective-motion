93a228b62f9f78cdf0fa641023ee50a3ebac72a6
Vincent Carpenter <vscarpen@isaac1.bc.rzg.mpg.de>
2019-12-04 14:13:34 +0100

    new runs!

---
+++ pencil-runs/1-particle_draglaw-simple_no-radius/submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Standard output and error:
+#SBATCH -o ./1-p_dls_nr.out.%j
+#SBATCH -e ./1-p_dls_nr.err.%j
+#SBATCH -D ./
+#SBATCH -J 1-p_dls_nr
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=40
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=carpenter@mpia.de
+# Wall clock limit
+#SBATCH --time=04:00:00
+
+pc_run -f $PENCIL_HOME/config/hosts/isaac/isaac1.bc.rzg.mpg.de.conf
