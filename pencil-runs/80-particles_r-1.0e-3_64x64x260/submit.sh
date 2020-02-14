cbb489be627b6018448054bcd19246b6961b5b06
Vincent Carpenter <vscarpen@isaac1.bc.rzg.mpg.de>
2020-02-14 14:28:26 +0100

    new runs yall

---
+++ pencil-runs/80-particles_r-1.0e-3_64x64x260/submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Standard output and error:
+#SBATCH -o ./80-p_r-1.0e-3_64x64x260.out.%j
+#SBATCH -e ./80-p_r-1.0e-3_64x64x260.err.%j
+#SBATCH -D ./
+#SBATCH -J 80-p_r-1.0e-3_64x64x260
+#SBATCH --nodes=4
+#SBATCH --tasks-per-node=40
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=all
+#SBATCH --mail-user=carpenter@mpia.de
+# Wall clock limit
+#SBATCH --time=12:00:00
+
+pc_run -f $PENCIL_HOME/config/hosts/isaac/isaac1.bc.rzg.mpg.de.conf
